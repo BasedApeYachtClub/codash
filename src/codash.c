4eaaf0aabaec5e103c6e84b7da6bdeb8490573f8
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-01-16 19:20:04 +0100

    Get it compiling

--- src/codash.c
+++ src/codash.c
@@ -2,13 +2,13 @@
 
 #define FOR_INDEX(I, N) for (size_t(I) = 0; (I) < (N); ++(I))
 
-void *find_first(void *xs, size_t xs_len, size_t x_size, int (*predicate)(void *))
+void *find_first(void *xs, size_t len, size_t size, int (*predicate)(void *))
 {
-    FOR_INDEX(i, xs_len)
+    FOR_INDEX(i, len)
     {
-        if (predicate(xs + i * x_size))
+        if (predicate(xs + i * size))
         {
-            return xs + i * x_size;
+            return xs + i * size;
         }
     }
     return NULL;
--- src/codash.h
+++ src/codash.h
@@ -1,15 +1,16 @@
 #include <stdbool.h>
+#include <stddef.h>
 
 void *find_first(void *xs, size_t len, size_t size, bool (*predicate)(void *));
-void *find_nth(void *xs, size_t len, size_t size, bool (*predicate)(void *), size_t n);
-size_t count(void *xs, size_t len, size_t size, bool (*predicate)(void *));
+//void *find_nth(void *xs, size_t len, size_t size, bool (*predicate)(void *), size_t n);
+//size_t count(void *xs, size_t len, size_t size, bool (*predicate)(void *));
 
 // TODO len is not number of bytes docs
-void map(void *dst, void *src, size_t dst_size, size_t src_size, size_t len, void (*f)(void *d, void *s));
-void filter(void *dst, void *src, size_t len, size_t size, bool (*predicate)(void *));
+//void map(void *dst, void *src, size_t dst_size, size_t src_size, size_t len, void (*f)(void *d, void *s));
+//void filter(void *dst, void *src, size_t len, size_t size, bool (*predicate)(void *));
 // calls filter
-void filter_inplace(void *xs, size_t len, size_t size, bool (*predicate)(void *));
-void reduce(void *start, void *xs, size_t len, size_t size, void (*reducer)(void *acc, void *x));
+//void filter_inplace(void *xs, size_t len, size_t size, bool (*predicate)(void *));
+//void reduce(void *start, void *xs, size_t len, size_t size, void (*reducer)(void *acc, void *x));
 // reduce_right
 // flatmap
 // foreach
