ca1f8d50e968727cb9f8ce5c4d5d79b69975e846
meipp <meipp@users.noreply.github.com>
2022-01-17 00:42:42 +0100

    Fix predicate return value in codash.c find_first

--- src/codash.c
+++ src/codash.c
@@ -1,8 +1,9 @@
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define FOR_INDEX(I, N) for (size_t(I) = 0; (I) < (N); ++(I))
 
-void *find_first(void *xs, size_t len, size_t size, int (*predicate)(void *))
+void *find_first(void *xs, size_t len, size_t size, bool (*predicate)(void *))
 {
     FOR_INDEX(i, len)
     {
