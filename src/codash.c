75feb554f1d8d967cec16cd66533589bce2a213e
meipp <meipp@users.noreply.github.com>
2022-01-17 03:18:25 +0100

    Add any, none

--- src/codash.c
+++ src/codash.c
@@ -26,3 +26,21 @@ bool all(void *xs, size_t len, size_t size, bool (*predicate)(void *))
         return predicate(xs) && all(xs + size, len - 1, size, predicate);
     }
 }
+
+bool any(void *xs, size_t len, size_t size, bool (*predicate)(void *))
+{
+
+    if (len == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return predicate(xs) || any(xs + size, len - 1, size, predicate);
+    }
+}
+
+bool none(void *xs, size_t len, size_t size, bool (*predicate)(void *))
+{
+    return !any(xs, len, size, predicate);
+}
--- src/codash.h
+++ src/codash.h
@@ -7,6 +7,10 @@ void *find_first(void *xs, size_t len, size_t size, bool (*predicate)(void *));
 
 bool all(void *xs, size_t len, size_t size, bool (*predicate)(void *));
 
+bool any(void *xs, size_t len, size_t size, bool (*predicate)(void *));
+
+bool none(void *xs, size_t len, size_t size, bool (*predicate)(void *));
+
 // TODO len is not number of bytes docs
 //void map(void *dst, void *src, size_t dst_size, size_t src_size, size_t len, void (*f)(void *d, void *s));
 //void filter(void *dst, void *src, size_t len, size_t size, bool (*predicate)(void *));
@@ -22,8 +26,6 @@ bool all(void *xs, size_t len, size_t size, bool (*predicate)(void *));
 // (cartesian foreach funccalls)
 // find_last
 // numpy level slicing to new array
-// none (literally !any)
-// any
 // fill (constant value, not byte)
 // TODO new name: fill_with_array (iterates through elements, allows dynamic array size to fill, sth sth modulo length)
 // reverse
